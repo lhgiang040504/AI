1a5ac8e3ca79907984e8daf2e34c6d5c19b876e9
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-24 14:28:33 +0700

    done 2.2

--- Python_Assignment/Part2_Map_Filter_Sorted/Map/map.py
+++ Python_Assignment/Part2_Map_Filter_Sorted/Map/map.py
@@ -1,12 +1,18 @@
+import math
 # Map for conversion data
 """
 During "conversion," data is often changed in both value and format.
 During "casting," the primary goal is to change the data type of the data while keeping the underlying value unchanged (in safe casting scenarios).
 """
-import math
-def conversion(input):
+def Convers(input):
     input = [float(element) for element in input]
     output = map(math.floor, input)
     return list(output)
-lst = input().split(" ")
-print(conversion(lst))
+
+def Spuare(input):
+    input = [int(element) for element in input]
+    output = map(lambda element : int(math.pow(element, 2)), input) # Can not same the abl=ove because pow need two argumants
+    return list(output)
+
+lst = input().split(" ") # The format way to input a list
+print(Spuare(lst))
