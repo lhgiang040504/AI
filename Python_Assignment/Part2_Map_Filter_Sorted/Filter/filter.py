06d34b671b8627f6ddd806ac7ddc4dff94315104
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-24 20:50:30 +0700

    done filter.2

--- Python_Assignment/Part2_Map_Filter_Sorted/Filter/filter.py
+++ Python_Assignment/Part2_Map_Filter_Sorted/Filter/filter.py
@@ -3,5 +3,10 @@ def get_PosNumbers(input):
     input = [int(element) for element in input]
     output = filter(lambda x : x > 0, input)
     return list(output)
+
+# Use filter to collect words that has first letter is capital
+def initial_capsWord(input):
+    output = filter(lambda x : x[0].isupper(), input)
+    return list(output)
 lst = input().split(" ")
-print(get_PosNumbers(lst))
+print(initial_capsWord(lst))
