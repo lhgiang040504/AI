ddf004bf4c0966a7efb51bf3ecdef01a49cf9a60
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-25 11:43:44 +0700

    done

--- Python_Assignment/Part2_Map_Filter_Sorted/Sorted/sorted.py
+++ Python_Assignment/Part2_Map_Filter_Sorted/Sorted/sorted.py
@@ -2,7 +2,22 @@
 "sort need integer"
 def descending_Sort(lst):
     lst = [int(element) for element in lst]
-    return sorted(lst)
+    return sorted(lst, reverse=True)
+"""
+Note that:
+    numbers = [5, 2, 9, 1, 5]
+    result = numbers.sort()
+    print(result)  # Output: None
+    print(numbers)  # Output: [1, 2, 5, 5, 9]
+"""
+# Reverse the order
+def reverse_Order(lst):
+    return lst[::-1]
 
 lst = input().split(" ")
-print(descending_Sort(lst))
+print(reverse_Order(lst))
+
+"""
+Note that:
+    format of each type of value is not same that may cause error
+"""
