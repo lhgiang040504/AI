b68e385a4810176236f7a9a006441239b4aea8ee
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-27 12:35:30 +0700

    done 1.1

---
+++ Python_Assignment/Part3_Dictionary_List/dictionary.py
@@ -0,0 +1,10 @@
+# Map two list that key list and value list to a dictionary
+def create_dictionary_from_lists(key_list, value_list):
+    dct = {}
+    for key, value in zip(key_list, value_list):
+        dct[key] = value
+    return dct
+
+key_list = input().split(" ")
+value_list = input().split(" ")
+print(create_dictionary_from_lists(key_list, value_list))
