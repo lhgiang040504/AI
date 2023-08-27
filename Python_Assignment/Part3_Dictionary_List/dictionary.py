fd82e33ffd752f8a9cb6c7071be7b010123ebf46
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-27 12:40:27 +0700

    map applycation

--- Python_Assignment/Part3_Dictionary_List/dictionary.py
+++ Python_Assignment/Part3_Dictionary_List/dictionary.py
@@ -6,5 +6,5 @@ def create_dictionary_from_lists(key_list, value_list):
     return dct
 
 key_list = input().split(" ")
-value_list = input().split(" ")
+value_list = map(int, input().split(" "))
 print(create_dictionary_from_lists(key_list, value_list))
