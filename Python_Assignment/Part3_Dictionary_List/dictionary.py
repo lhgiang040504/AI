fefb8ef8c2ce14d3558b7e0d03492417ee13b999
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-27 12:58:09 +0700

    done 1.2

--- Python_Assignment/Part3_Dictionary_List/dictionary.py
+++ Python_Assignment/Part3_Dictionary_List/dictionary.py
@@ -5,6 +5,15 @@ def create_dictionary_from_lists(key_list, value_list):
         dct[key] = value
     return dct
 
-key_list = input().split(" ")
-value_list = map(int, input().split(" "))
-print(create_dictionary_from_lists(key_list, value_list))
+# Check multiple keys exists in a dictionary
+def check_Key(dct, lst):
+    for element in lst:
+        if not (element in dct):
+            return False
+    return True
+
+dct = {"key_1": "value_1", "key_2": "value_2", "key_3": "value_3"}
+lst1 = ["key_1", "key_4"]
+lst2 = ["key_2", "key_3"]
+print(check_Key(dct, lst1))
+print(check_Key(dct, lst2))
