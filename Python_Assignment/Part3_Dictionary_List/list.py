b0a18f5c2af7ccb082cd55aa0d662d3f5ad0fa79
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-27 14:41:07 +0700

    done 2.4

--- Python_Assignment/Part3_Dictionary_List/list.py
+++ Python_Assignment/Part3_Dictionary_List/list.py
@@ -16,7 +16,12 @@ def following_orderConcatenate_Lists(lst1, lst2):
 def IterateDoubleDirection(lst1, lst2):
     result = list(zip(lst1, reversed(lst2)))
     return result
-lst_1 = ["Hello", "World"]
-lst_2 = ["Dear", "Sir"]
-print(IterateDoubleDirection(lst_1, lst_2))
+
+# Remove empty strings from the list of strings by using `filter` function
+def remove_emptyStringsMember(lst):
+    result = filter(lambda x : x != "", lst)
+    return list(result)
+lst_1 = ["10", "20", "30", "", "40"]
+
+print(remove_emptyStringsMember(lst_1))
 
