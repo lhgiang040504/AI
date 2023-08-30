975526bff5968ace2fc1dcf51b59a5ed5e60e933
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-30 12:39:50 +0700

    override some operator(+, -, *, /)

--- Python_Assignment/Part4_OOP/rational_number.py
+++ Python_Assignment/Part4_OOP/rational_number.py
@@ -7,7 +7,7 @@ class ZeroDenominatorError(Exception):
 
 class RationalNumber:
     def __init__(self, nume, deno=1):
-        if not (isinstance(nume, int) and isinstance(deno, int)):
+        if not isinstance(nume, int) or not isinstance(deno, int):
             raise NotIntegerError("NotIntegerError")
         if deno == 0:
             raise ZeroDenominatorError("ZeroDenomiator")
@@ -17,7 +17,7 @@ class RationalNumber:
 
     def simplify(self):
         gcd = math.gcd(self.nume, self.deno)
-        return RationalNumber(self.nume / gcd, self.deno / gcd)
+        return RationalNumber(self.nume // gcd, self.deno // gcd) # Noted that we need // to get rid of error
     
     def __str__(self):
         self = self.simplify()
@@ -25,3 +25,21 @@ class RationalNumber:
             return f"{self.nume}"
         else:
             return f"{self.nume} / {self.deno}"
+
+    def __add__(self, other):
+        denominator = (self.deno * other.deno)
+        numerator = (self.nume * other.deno) + (self.deno * other.nume)
+        return RationalNumber(numerator, denominator).simplify()
+    def __sub__(self, other):
+        denominator = (self.deno * other.deno)
+        numerator = (self.nume * other.deno) - (self.deno * other.nume)
+        return RationalNumber(numerator, denominator).simplify()
+    def __mul__(self, other):
+        return RationalNumber(self.nume * other.nume, self.deno * other.deno).simplify()
+    def __truediv__(self, other):
+        return RationalNumber(self.nume * other.deno, self.deno * other.nume).simplify()
+
+
+a = RationalNumber(4, 5)
+b = RationalNumber(6, 5)
+print(a / b)
