e8d2435011d06851da14d94427c268697346abd1
lhgiang040504 <lhgiang040504@gmail.com>
2023-08-31 14:04:33 +0700

    add exponentialBy and some noticed

--- Python_Assignment/Part6_Generator/generator.py
+++ Python_Assignment/Part6_Generator/generator.py
@@ -31,7 +31,24 @@ class multipleBy:
     def __next__(self):
         self.curr += self.factor
         return self.curr
-
+"""
 multiply = multipleBy(5)
 for i in range(100):
     print(next(multiply))
+"""
+
+def exponentialBy(exponent):
+    curr = 1
+    while not (curr > 100):
+        yield curr ** exponent
+        curr += 1
+"""
+In the context of your exponentialBy generator, each time you call next(exponential), the generator function exponentialBy(n) calculates the next value and yields it. 
+The function's state is then saved until the next iteration. 
+This allows you to generate values on-the-fly without having to store them all in memory at once.
+"""
+exponential = exponentialBy(3)
+
+print(next(exponential))  # Output: 1
+print(next(exponential))  # Output: 8
+print(next(exponential))  # Output: 27
