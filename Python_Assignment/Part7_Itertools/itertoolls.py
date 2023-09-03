bb50a7f672f5baceb4e217cd2395c5d0c75240cd
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-03 11:05:41 +0700

    add some prevVer comment and the professional version

--- Python_Assignment/Part7_Itertools/itertoolls.py
+++ Python_Assignment/Part7_Itertools/itertoolls.py
@@ -1,9 +1,11 @@
 #1. Use `itertools` to create groups of similar items of a given list.
 def group_BySimilar1(lst):
+    # Create a dictionary to store the groups
     groups = {}
+
+    # Group items by last name
     for element in lst:
         lastName = element.split()[-1]
-
         if lastName not in groups:
             groups[lastName] = []
         groups[lastName].append(element)
@@ -14,7 +16,25 @@ Noticed:
     "group" container must be dictionary of this way
     split() equivalent split(" ")
 
+    lst = ["Thomas Brown", "Tom Smith", "Jane Brown", "John Smith"]
+    
+    # Convert the dictionary values to a list to get the final result
+    result = list(group_BySimilar1(lst).values())
+    
+    print(result)
+"""
+from itertools import groupby
+def group_BySimilar2(lst):
+    # Sort the input list by last name
+    lst.sort(key = lambda x: x.split()[-1]) # not clear
+
+    groups = []
+    # Use itertools.groupby to create groups based on the last name
+    for key, group in groupby(lst, key = lambda x: x.split()[-1]):
+        groups.append(list(group))
+
+    return groups
+"""
 lst = ["Thomas Brown", "Tom Smith", "Jane Brown", "John Smith"]
-result = list(group_BySimilar1(lst).values())
-print(result)
+print(group_BySimilar2(lst))
 """
