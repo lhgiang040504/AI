5100b764be36ef3b72144d3768728c9bc6890108
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-03 13:12:47 +0700

    add two last problem extend list and combine by length use itertools

--- Python_Assignment/Part7_Itertools/itertoolls.py
+++ Python_Assignment/Part7_Itertools/itertoolls.py
@@ -43,7 +43,7 @@ print(group_BySimilar2(lst))
 from itertools import chain
 def combine_List(lst_1, lst_2):
     # Combine the lists using itertools.chain
-    return list(chain(lst_1 + lst_2))
+    return list(chain(lst_1, lst_2))
 # Before combine, we need extract the nested list
 def flatten_list(lst):
     result = []
@@ -77,10 +77,46 @@ def combine_Unique2(lst):
 
     # Format the combinations as strings
     return [" - ".join(combination) for combination in unique_combinations]
-
-# unique_combinations = [f"{a} - {b}" for i, a in enumerate(input_list) for j, b in enumerate(input_list) if i < j]
 """
+# unique_combinations = [f"{a} - {b}" for i, a in enumerate(input_list) for j, b in enumerate(input_list) if i < j]
+
 combination
 permutation
-"""
+
 print(combine_Unique2(["Red", "Yellow", "Green"]))
+"""
+
+#4. Use `itertools` to group all iterables together and produce a single iterable
+def combine_List1(*arg):
+    # Combine the lists using itertools.chain
+    return list(chain(*arg))
+def combine_List2(*arg):
+    result = []
+    for lst in arg:
+        result.extend(lst)
+    return result
+"""
+lst_1 = [1, 4, 7, 10, 13, 16]
+lst_2 = [2, 5, 8, 11, 14, 17]
+lst_3 = [3, 6, 9, 12, 15, 18]
+
+print(combine_List2(lst_1, lst_2, lst_3))
+"""
+
+#5. Use `itertools` to display elements in an iterable in the group of length
+def combine_ByLength(lst):
+    lst.sort(key = lambda x: len(x))
+
+    groups = {}
+    # Use itertools.groupby to create groups based on the last name
+    for key, group in groupby(lst, key = lambda x: len(x)):
+        groups[key] = list(group)
+    return dict(groups)
+
+lst = ["keep", "smiling", "because", "life",
+        "is", "a", "beautiful", "thing",
+          "and", "there", "is", "so",
+            "much", "to", "smile", "about"]
+result = combine_ByLength(lst)
+for key, group in result.items():
+    print(f"Key: {key} - Group: {group}")
