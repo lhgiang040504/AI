0edc1c9dba9c7ec0b920c59ef5c8c99f1cd9ea34
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-04 15:36:02 +0700

    still practice

--- Python_Assignment/Part10_Decorator/decorator.py
+++ Python_Assignment/Part10_Decorator/decorator.py
@@ -1,5 +1,4 @@
 from functools import wraps
-
 def login_Required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -14,10 +13,21 @@ def login_Required(func):
                 print(f"Login failed. {attemps - 1} attemps left")
                 attemps -= 1
         print("You have exceeced the maximum number of login attemps.")
-    return wrapper # not clear
+    return wrapper # not
 
 @login_Required
 def protected_Function():
     print("Successful access")
 
-protected_Function()
+#protected_Function()
+
+import time
+def measure_execution_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time} seconds to execute.")
+        return result
+    return wrapper
