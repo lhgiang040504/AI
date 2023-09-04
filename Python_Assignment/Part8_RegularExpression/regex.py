e3722164b0f7a01026f90e0369014b78ee2225ae
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-04 12:56:42 +0700

    add some explaination

--- Python_Assignment/Part8_RegularExpression/regex.py
+++ Python_Assignment/Part8_RegularExpression/regex.py
@@ -14,3 +14,16 @@ def is_valid_email(email):
 print(is_valid_email("jayson@gmail.com"))       # True
 print(is_valid_email("jayson_123@gmail.com"))   # True
 print(is_valid_email("jayson-54@gmail.com.vn")) # False
+
+"""
+Functions in the re module:
+
+    re.match(pattern, string): Attempts to match the pattern at the beginning of the string. 
+Returns a match object if successful, or None if no match is found.
+    re.search(pattern, string): Searches the entire string for a match to the pattern. 
+Returns a match object if a match is found, or None if not.
+    re.findall(pattern, string): Returns all non-overlapping matches as a list of strings.
+    re.finditer(pattern, string): Returns an iterator yielding match objects for all non-overlapping matches.
+    re.sub(pattern, replacement, string): Replaces all occurrences of the pattern in the string with the replacement string.
+    re.compile(pattern): Compiles a regular expression pattern into a regex object, which can be reused for multiple searches or matches.
+"""
