6ac0fadcdff91bcca977e7550f7727c8704c5934
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-06 13:04:18 +0700

    manual model

---
+++ Machine_Learning/Supervised_Learning/Linear_Regression/simple/simple_LinearRegression.py
@@ -0,0 +1,22 @@
+def compute_CoefficientModel(x_train, y_train):
+    length = len(x_train)
+    
+    # Calculate the mean of X and Y
+    mean_x = sum(x_train) / length
+    mean_y = sum(y_train) / length
+
+    # Compute the slope and intercept/bias
+
+    numerator = sum((x_train[i] - mean_x) * (y_train[i] - mean_y) for i in range(length))
+    denominator = sum((x_train[i] - mean_x)**2 for i in range(length))
+
+    slope = numerator / denominator
+    intercept = mean_y - slope * mean_x
+
+    return [slope, intercept]
+
+def predict_LableModel(x_test, coefficient):
+    slope = coefficient[0]
+    intercept = coefficient[-1]
+
+    return x_test * slope + intercept
