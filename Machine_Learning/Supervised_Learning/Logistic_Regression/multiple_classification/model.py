76e752f628a221b316833f5f30b7999a7d31f845
lhgiang040504 <lhgiang040504@gmail.com>
2023-09-23 19:59:44 +0700

    professional version, honestly rename variable and update some basic structures

--- Machine_Learning/Supervised_Learning/Logistic_Regression/multiple_classification/model.py
+++ Machine_Learning/Supervised_Learning/Logistic_Regression/multiple_classification/model.py
@@ -1,37 +1,37 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, learninng_rate=0.1, num_iters=1000):
-        self.learning_rate  = learninng_rate
-        self.num_iters      = num_iters
-        self.coefficent     = None
-        self.classes        = None
+    def __init__(self, learning_rate=0.1, num_iters=1000):
+        self.learning_rate = learning_rate
+        self.num_iters = num_iters
+        self.coefficient = None
+        self.classes = None
     
     def _add_bias(self, features):
         """
-        Add bias column to the features matrix.
+        Add a bias column to the features matrix.
         
         Args:
-            features: np.array of shape (observers, features)
+            features: np.array of shape (observations, features)
         
         Returns:
-            np.array of shape (observers, features + 1): features matrix with bias column added
+            np.array of shape (observations, features + 1): features matrix with a bias column added
         """
         bias = np.ones((features.shape[0], 1))
-        features = np.concatenate((features, bias), axis=1) # update x that has bias column at the last column
-        return features
+        features_with_bias = np.concatenate((features, bias), axis=1)
+        return features_with_bias
 
     def _softmax(self, z):
         """
         Compute the softmax function for the given array of scores.
         
         Args:
-            z: np.array of shape (observers, num_classes)
+            z: np.array of shape (observations, num_classes)
         
         Returns:
-            np.array of shape (observers, num_classes): softmax probabilities
+            np.array of shape (observations, num_classes): softmax probabilities
         """
-        exp_z = np.exp(z)
+        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
         return exp_z / np.sum(exp_z, axis=1, keepdims=True)
     
     def _one_hot_encoding(self, labels):
@@ -39,14 +39,13 @@ class LogisticRegression:
         Perform one-hot encoding for the given array of labels.
         
         Args:
-            labels: np.array of shape (observers, 1) with string labels
+            labels: np.array of shape (observations, 1) with string labels
         
         Returns:
-            np.array of shape (observers, num_classes): one-hot encoded labels
+            np.array of shape (observations, num_classes): one-hot encoded labels
         """
         num_classes = len(self.classes)
-        num_observations = labels.shape[0]
-        encoded = np.zeros((num_observations, num_classes))
+        encoded = np.zeros((labels.shape[0], num_classes))
         for i, label in enumerate(labels):
             encoded[i, label] = 1
         return encoded
@@ -56,37 +55,37 @@ class LogisticRegression:
         Fit the logistic regression model to the training data.
         
         Args:
-            observations: np.array of shape (observers, features) representing the training data
-            labels: np.array of shape (observers, 1) representing the training labels
+            observations: np.array of shape (observations, features) representing the training data
+            labels: np.array of shape (observations, 1) representing the training labels
         """
         self.classes = np.unique(labels)
         num_features = observations.shape[1]
         num_classes = len(self.classes)
 
-        self.coefficent = np.zeros((num_classes, num_features + 1)) # (num_classes, num_features + 1)
+        self.coefficient = np.zeros((num_classes, num_features + 1))  # (num_classes, num_features + 1)
 
-        observations = self._add_bias(observations) # (obsers, num_features + 1)
-        labels = self._one_hot_encoding(labels)
+        observations_with_bias = self._add_bias(observations)  # (observations, num_features + 1)
+        labels_encoded = self._one_hot_encoding(labels)
         
         for _ in range(self.num_iters):
-            scores = np.dot(observations, self.coefficent.T) # (obsers, num_classes)
-            probabilities = self._softmax(scores) # (obsers, num_classes)
+            scores = np.dot(observations_with_bias, self.coefficient.T)  # (observations, num_classes)
+            probabilities = self._softmax(scores)  # (observations, num_classes)
             
-            error = probabilities - labels
-            gradient = np.dot(error.T, observations) # (num_classes, num_features + 1)
-            self.coefficent -= self.learning_rate * gradient
+            error = probabilities - labels_encoded
+            gradient = np.dot(error.T, observations_with_bias)  # (num_classes, num_features + 1)
+            self.coefficient -= self.learning_rate * gradient
            
     def predict(self, observations):
         """
         Predict the class labels for the given observations.
         
         Args:
-            observations: np.array of shape (observers, features) representing the test data
+            observations: np.array of shape (observations, features) representing the test data
         
         Returns:
-            np.array of shape (observers,): predicted class labels
+            np.array of shape (observations,): predicted class labels
         """
-        observations = self._add_bias(observations)
-        scores = np.dot(observations, self.coefficent.T)
+        observations_with_bias = self._add_bias(observations)
+        scores = np.dot(observations_with_bias, self.coefficient.T)
         probabilities = self._softmax(scores)
         return np.argmax(probabilities, axis=1)
