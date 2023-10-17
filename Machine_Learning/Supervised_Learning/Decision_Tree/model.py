39a3a7d1e984a93161cf2eb45e40665fce7304da
lhgiang040504 <lhgiang040504@gmail.com>
2023-10-18 00:40:34 +0700

    rebuild-model build an outline of model

--- Machine_Learning/Supervised_Learning/Decision_Tree/model.py
+++ Machine_Learning/Supervised_Learning/Decision_Tree/model.py
@@ -1,170 +1,74 @@
 import numpy as np
 
-class Decision_Tree:
-    def __init__(self, max_depth=None):
+class Node:
+    def __init__(self, feature=None, threshold=None, left_subtree=None, right_subtree=None, *,value):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_subtree = left_subtree
+        self.right_subtree = right_subtree
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, max_depth=10, min_sample=2):
         self.max_depth = max_depth
-        self.tree = {}
-        print("hello world")
+        self.min_sample = min_sample
+        self.root = None
 
-    # CART  -----------
-    def gini(self, y):
-        """
-        parameters: 
-            y : Label of observations based on specified features within threshold.
-        return:
-            gini_impurity : Impurity values of y.
-        """
-        # Calculate the Gini Impurity of a target variable
-        classes, counts = np.unique(y, return_counts=True)
-        probabilities = counts / len(y)
-        gini_impurity = 1 - np.sum(probabilities ** 2)
-        
-        return gini_impurity
-    
-    def gini_index(self, X, y, feature_idx, threshold):
-        """
-        parameters:
-            X : The feature matrix, where each row represents a sample and each column represents a feature.
-            y : The target variable, which contains the corresponding class labels for each sample.
-            feature_idx : The index of the feature for which the Gini index is calculated.
-            threshold :  The threshold value used to split the data based on the specified feature.
-        return:
-            gini_index : The gini INdex of the split
-
-        """
-        # Split the data based on the given threshold and feature
-        left_mask = X.iloc[:, feature_idx] <= threshold
-        right_mask = X.iloc[:, feature_idx] > threshold
-        y_left = y[left_mask]
-        y_right = y[right_mask]
-
-        # Calculate the Gini Impurity of the left and right subsets
-        gini_left = self.gini(y_left)
-        gini_right = self.gini(y_right)
-
-        # Calculate the Gini Index
-        len_left = len(y_left)
-        len_right = len(y_right)
-        len_total = len(y)
-        gini_index = (len_left / len_total) * gini_left + (len_right / len_total) * gini_right
-        
-        return gini_index
-    
-    # Classification and Regression Trees
-    def CART_find_Best_Split(self, X, y):
-        best_gini_index = np.inf
-        best_feature = None
-        best_threshold = None
-
-        for feature_idx in range(X.shape[1]):
-            thresholds = np.unique(X.iloc[:, feature_idx])
-            for threshold in thresholds:
-                gini_index = self.gini_index(X, y, feature_idx, threshold)
-
-                if gini_index < best_gini_index:
-                    best_gini_index = gini_index
-                    best_feature = feature_idx
-                    best_threshold = threshold
-
-        return best_feature, best_threshold
-
-    # C45 - ID3     ----------
-    def entropy(self, y):
-        # Calculate the entropy of a target variable
-        classes, counts = np.unique(y, return_counts=True)
-        probabilities = counts / len(y)
-        entropy = -np.sum(probabilities * np.log2(probabilities))
-        
-        return entropy
-
-    def information_gain(self, X, y, feature_idx, threshold):
-        # Calculate the information gain for a given feature and threshold
-        entropy_parent = self.entropy(y)
-
-        # Split the data based on the given feature and threshold
-        left_mask = X.iloc[:, feature_idx] <= threshold
-        right_mask = X.iloc[:, feature_idx] > threshold
-        y_left = y[left_mask]
-        y_right = y[right_mask]
-
-        # Calculate the entropy of the left and right subsets
-        entropy_left = self.entropy(y_left)
-        entropy_right = self.entropy(y_right)
-
-        # Calculate the information gain
-        n_left = len(y_left)
-        n_right = len(y_right)
-        n_total = len(y)
-        information_gain = entropy_parent - (n_left / n_total * entropy_left) - (n_right / n_total * entropy_right)
-        return information_gain
-
-    def C45_find_Best_Split(self, X, y):
-        best_InforGain = 0
-        best_feature = None
-        best_threshold = None
-
-        for feature_idx in range(X.shape[1]):
-            thresholds = np.unique(X.iloc[:, feature_idx])
-            for threshold in thresholds:
-                infor_gain = self.information_gain(X, y, feature_idx, threshold)
-
-                if infor_gain > best_InforGain:
-                    best_InforGain = infor_gain
-                    best_feature = feature_idx
-                    best_threshold = threshold
-
-        return best_feature, best_threshold
-
-    
     def fit(self, X, y):
-        self.tree = self.build_tree(X, y)
+        self._build_tree(X, y)
 
-    def build_tree(self, X, y, depth=0):
-        # Base case: If all samples belong to the same class, return a leaf node
-        if len(np.unique(y) == 1) or (self.max_depth is not None and depth == self.max_depth):
-            return {'class' : y[0]}
+    def _build_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
         
+        # Base case: If all samples belong to the same class, return a leaf node
+        if len(n_labels == 1):
+            return Node(value=np.unique(y))
+
+        # Base case : If n_sample reaches the min_sample or max_depth is exceeded, do the majority vote
+        if (n_samples <= self.min_sample) or (depth == self.max_depth):
+            label_frequency = np.unique(y, return_counts=True)
+            majority_label = label_frequency[0][np.argmax(label_frequency[1])]
+            return Node(value=majority_label)
+
         # Find the best feature and threshold to split the data
-        best_feature, best_threshold = self.find_best_split(X, y)
+        best_feature, best_threshold = self._find_Best_Split(X, y)
 
         # Split the data based on the best feature and best threshold
-        left_mask = X.iloc[:, best_feature] <= best_threshold
-        right_mask = X.iloc[:, best_feature] > best_threshold
+        X_left, y_lelf, X_right, y_right = self._split(X, y, best_feature, best_threshold)
+
+        # Recursive call to build tree
+        left_subtree = self._build_tree(X_left, y_lelf, depth + 1)
+        right_subtree = self._build_tree(X_right, y_right, depth + 1)
+
+        # Create the current node with the best feature and best threshold
+        node = Node(best_feature, best_threshold, left_subtree, right_subtree)
+       
+        return node
+
+    def _find_Best_Split(self):
+        pass
+
+    def _split(self, X, y, best_feature, best_threshold):
+        left_mask = X[:, best_feature] <= best_threshold
+        right_mask = X[:, best_feature] > best_threshold
         X_left, y_left = X[left_mask], y[left_mask]
         X_right, y_right = X[right_mask], y[right_mask]
 
-        # Recursive call to build tree
-        left_subtree = self.build_tree(X_left, y_left, depth + 1)
-        right_subtree = self.build_tree(X_right, y_right, depth + 1)
-
-        # Create the current node with the best feature and best threshold
-        node = {
-            'feature' : best_feature,
-            'threshold' : best_threshold,
-            'left_subtree' : left_subtree,
-            'right_subtree' : right_subtree
-        }
-
-        return node
-    
+        return X_left, y_left, X_right, y_right
+         
     def predict(self, X):
         # Predict the class for each instance in X
-        return np.array([self.traverse_tree(x, self.tree) for x in X.values])
+        return np.array([self._traverse_tree(x, self.root) for x in X])
     
-    def traverse_tree(self, x, node):
-        """
-        parameters:
-            x : An observation
-            node : Node in tree
-        return:
-            label : Label prediction of instance observation
-        """
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
         
-        # If the current node is a leaf node (it has a 'class' attribute), return the predicted class
-        if 'class' in node:
-            return node['class']
-
-        if x[node['feature']] <= node['threshold']:
-            return self.traverse_tree(x, node['left_subtree'])
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.lelf_subtree)
         else:
-            return self.traverse_tree(x, node['right_subtree'])
+            return self._traverse_tree(x, node.right_subtree)
