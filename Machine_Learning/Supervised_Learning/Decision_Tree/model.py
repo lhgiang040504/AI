2227d70d1e87ac6dcafcec7bdd55751f4ec7f672
lhgiang040504 <lhgiang040504@gmail.com>
2023-10-18 02:31:25 +0700

    done the model's helper function which is ID3 use entropy and informaion gain. Already check bug and some error of syntax.

--- Machine_Learning/Supervised_Learning/Decision_Tree/model.py
+++ Machine_Learning/Supervised_Learning/Decision_Tree/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Node:
-    def __init__(self, feature=None, threshold=None, left_subtree=None, right_subtree=None, *,value):
+    def __init__(self, feature=None, threshold=None, left_subtree=None, right_subtree=None, *, value = None):
         self.feature = feature
         self.threshold = threshold
         self.left_subtree = left_subtree
@@ -18,14 +18,14 @@ class DecisionTree:
         self.root = None
 
     def fit(self, X, y):
-        self._build_tree(X, y)
+        self.root = self._build_tree(X, y)
 
     def _build_tree(self, X, y, depth=0):
         n_samples, n_features = X.shape
         n_labels = len(np.unique(y))
         
         # Base case: If all samples belong to the same class, return a leaf node
-        if len(n_labels == 1):
+        if n_labels == 1:
             return Node(value=np.unique(y))
 
         # Base case : If n_sample reaches the min_sample or max_depth is exceeded, do the majority vote
@@ -35,10 +35,12 @@ class DecisionTree:
             return Node(value=majority_label)
 
         # Find the best feature and threshold to split the data
-        best_feature, best_threshold = self._find_Best_Split(X, y)
+        best_feature, best_threshold = self._find_best_split(X, y)
 
         # Split the data based on the best feature and best threshold
-        X_left, y_lelf, X_right, y_right = self._split(X, y, best_feature, best_threshold)
+        left_mask, right_mask = self._split(X, y, best_feature, best_threshold)
+        X_left, y_lelf = X[left_mask], y[left_mask]
+        X_right, y_right = X[right_mask], y[right_mask]
 
         # Recursive call to build tree
         left_subtree = self._build_tree(X_left, y_lelf, depth + 1)
@@ -49,17 +51,65 @@ class DecisionTree:
        
         return node
 
-    def _find_Best_Split(self):
-        pass
-
     def _split(self, X, y, best_feature, best_threshold):
         left_mask = X[:, best_feature] <= best_threshold
         right_mask = X[:, best_feature] > best_threshold
-        X_left, y_left = X[left_mask], y[left_mask]
-        X_right, y_right = X[right_mask], y[right_mask]
 
-        return X_left, y_left, X_right, y_right
-         
+        return left_mask, right_mask
+    
+    def _find_best_split(self, X, y):
+        return self._ID3_find_best_split(X, y)
+    
+    # ---- ID3 algorithm to find best property for splitting
+    def _entropy(self, y):
+        # Calculate the entropy of a target variable
+        bincounts = np.bincount(y)
+        probabilities = bincounts / len(y)
+        result = -np.sum([probability * np.log(probability) for probability in probabilities if np.greater(probability, 0)])
+        return result
+    
+    def _information_gain(self, X, y, feature_idx, threshold):
+        # Calculate entropy of parent node
+        parent_entropy = self._entropy(y)
+
+        # Separate data into two groups by the given feature and threshold
+        left_mask, right_mask = self._split(X, y, feature_idx, threshold)
+        y_left = y[left_mask]
+        y_right = y[right_mask]
+        if len(y_left) * len(y_right) == 0:
+            return 0
+
+        # Calculate the entropy of the left and right subsets
+        entropy_left = self._entropy(y_left)
+        entropy_right = self._entropy(y_right)
+
+        # Calculate the information gain
+        n_left = len(y_left)
+        n_right = len(y_right)
+        n_total = len(y)
+        information_gain = parent_entropy - (n_left / n_total * entropy_left) - (n_right / n_total * entropy_right)
+        
+        return information_gain
+    
+    def _ID3_find_best_split(self, X, y):
+        best_InforGain = -np.inf
+        best_feature = None
+        best_threshold = None
+
+        for feature_idx in range(X.shape[1]):
+            thresholds = np.unique(X[:, feature_idx])
+            
+            for threshold in thresholds:
+                infor_gain = self._information_gain(X, y, feature_idx, threshold)
+
+                if infor_gain > best_InforGain:
+                    best_InforGain = infor_gain
+                    best_feature = feature_idx
+                    best_threshold = threshold
+
+        return best_feature, best_threshold
+    # ----
+
     def predict(self, X):
         # Predict the class for each instance in X
         return np.array([self._traverse_tree(x, self.root) for x in X])
@@ -69,6 +119,6 @@ class DecisionTree:
             return node.value
         
         if x[node.feature] <= node.threshold:
-            return self._traverse_tree(x, node.lelf_subtree)
+            return self._traverse_tree(x, node.left_subtree)
         else:
             return self._traverse_tree(x, node.right_subtree)
