95528d571b92df9563aaa855e363ec2e6631b869
lhgiang040504 <lhgiang040504@gmail.com>
2023-10-27 08:08:38 +0700

    add bootstrap dataset function

--- Machine_Learning/Supervised_Learning/Random_Forest/RandomForest.py
+++ Machine_Learning/Supervised_Learning/Random_Forest/RandomForest.py
@@ -6,12 +6,30 @@ class RandomForest:
         self.n_trees = n_trees
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
+        self.forest = None
 
     def fit(self, X, y):
-        pass
+        self.forest = []
+        for _ in range(self.n_trees):
+            # Call the Decisiontree model
+            tree = DecisionTree(max_depth=self.max_depth, 
+                                min_sample=self.min_samples_split, 
+                                helper_func="ID3")
+            # Bootstrap data
+            X_bootstrap, y_bootstrap = self._boosttrap_data(X, y)
+            
+            # Build tree, a part of n_trees
+            tree.fit(X_bootstrap, y_bootstrap)
+
+            # Add new tree to forest
+            self.forest.append(tree)
+
+
 
     def _boosttrap_samples(self, X, y):
-        pass
+        n_samples = X.shape[0]
+        indices = np.random.randint(0, n_samples, size=(n_samples))
+        return X[indices], y[indices]
 
     def _most_common_label(self):
         pass
