608c7be053091d0dc36d80d4bdd924738c58e64e
lhgiang040504 <lhgiang040504@gmail.com>
2023-11-01 09:16:08 +0700

    complete apply and understand model's error, syntax error

--- Machine_Learning/Supervised_Learning/Random_Forest/apply.py
+++ Machine_Learning/Supervised_Learning/Random_Forest/apply.py
@@ -8,15 +8,20 @@ df = pd.read_csv("../Decision_Tree/data/data.csv")
 y = df['Classification']
 X = df.drop(columns = 'Classification')
 
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values.reshape(-1, 1), test_size=0.2, random_state=42)
+from sklearn import datasets
+data = datasets.load_breast_cancer()
+X = data.data  # we only take the first two features.
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
 
-clf = RandomForest(n_trees=20)
+clf = RandomForest(n_trees=5)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
-acc =  accuracy(y_test, predictions)
+acc = accuracy(y_test, predictions)
 print(acc)
