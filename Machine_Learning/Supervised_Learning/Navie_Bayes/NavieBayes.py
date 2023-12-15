b0a5cf6bdd9fecd2f0af35148b900d17ca3f9a99
lhgiang040504 <lhgiang040504@gmail.com>
2023-12-15 21:52:00 +0700

    complete

--- Machine_Learning/Supervised_Learning/Navie_Bayes/NavieBayes.py
+++ Machine_Learning/Supervised_Learning/Navie_Bayes/NavieBayes.py
@@ -20,54 +20,46 @@ class NavieBayes:
     def __init__(self):
         print("Accept")
 
-
     def fit(self, X, y):
-        self._labels = len(np.unique(y))
+        self._labels = np.unique(y)
         self._samples, self._features = X.shape
-        # A small value eps used for numerical stability
         self.eps = 1e-6
         
         self.labels_mean = {}
         self.labels_variance = {}
         self.labels_prior = {}
 
-        for label in range(self._labels):
+        for label in self._labels:
             ident_label = X[y == label]
 
             # Calculate mean, variance and probability of each label in trainset
-            self.labels_mean[str(label)] = np.mean(ident_label, axis=0)
-            self.labels_variance[str(label)] = np.mean(ident_label, axis=0)
-            self.labels_prior[str(label)] = ident_label.shape[0] / self._samples
+            self.labels_mean[int(label)] = np.mean(ident_label, axis=0)
+            self.labels_variance[int(label)] = np.var(ident_label, axis=0)
+            self.labels_prior[int(label)] = ident_label.shape[0] / self._samples
+            
+        print(self.labels_mean)
+        print(self.labels_variance)
+        print(self.labels_prior)
 
-    def predeict(self, X):
-        probs = np.zeros((self._samples, self._labels))
+    def predict(self, X):
+        probs = np.zeros((self._samples, len(self._labels)))
 
-        for label in range(self._labels):
+        for idx, label in enumerate(self._labels):
             # Captute all necessaty informations of each label
-            mean = self.labels_mean[str(label)]
-            variance = self.labels_variance[str(label)]
-            prior = self.labels_prior[str(label)]
+            mean = self.labels_mean[int(label)] # (1, num_features)
+            variance = self.labels_variance[int(label)] # (1, num_feature)
+            prior = self.labels_prior[int(label)] # (1, 1)
 
             # Calculate posterior for each class
             probs_label = self.Gaussian_Formular(X, mean, variance)
-            probs[:, label] = probs_label + np.log(prior)
-
-        return np.argmax(probs_label, 1)
+            probs[:, idx] = probs_label + np.log(prior)
+                    
+        return np.argmax(probs, axis=1)
     
     def Gaussian_Formular(self, X, mean, variance):
         # Calculate probability from Gaussian density function
-        const = -self._features / 2* np.log(2 * np.pi) - 0.5 * np.sum(np.log(variance + self.eps))
+        numerator = np.exp(-((X - mean) ** 2 / (2 * variance)))
+        denominator = np.sqrt(2 * np.pi * variance)
+        posterior = np.sum(np.log(numerator / denominator), axis=1)
         
-        probs = 0.5 * np.sum(np.power(X - mean, 2) / (variance + self.eps), 1)
-        return const - probs
-
-
-if __name__ == "__main__":
-    X = np.loadtxt("data/data.txt", delimiter=",")
-    y = np.loadtxt("data/targets.txt") - 1
-
-    NB = NavieBayes()
-    NB.fit(X, y)
-    y_pred = NB.predeict(X)
-
-    print(f"Accuracy: {sum(y_pred==y)/X.shape[0]}")
+        return posterior
